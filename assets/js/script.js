2eab9519c22a6d843ec2bfc7d2a5a7c430d0f052
riicky-hub <richardjosephace1960@gmail.com>
2022-07-07 21:50:56 -0300

    adicionando animacoes e novo projeto(caculadora)

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,8 @@ window.onscroll = function() {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progressBar.style.height = progressHeight + "%";
 }
+// AOS - Animation
+AOS.init();
+// Popper(Bootstrap)
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
